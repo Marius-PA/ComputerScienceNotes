40fddf99fcbb412f34b12d253486e81e677de8f4
TheBlueNeon <marius.poncon@gmail.com>
2023-09-18 15:41:10 +0100

    Update code.py

--- HackerRank/challenge_2/code.py
+++ HackerRank/challenge_2/code.py
@@ -1,3 +1,11 @@
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
+
+# first line = the sum of the two numbers.
+# second line = the difference of the two numbers (first - second).
+# third line = the product of the two numbers.
+
+print(a + b)
+print(a - b)
+print(a * b)
